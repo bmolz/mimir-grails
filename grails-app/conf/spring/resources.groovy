cc3a394122945fd612519e6b15867ca1a4a33b28
Brendan <bjmoeller@gmail.com>
2018-09-26 18:57:52 -0400

    Add Spring Security
    grails s2-quickstart com.bmolz.mimir User Role

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import mimir.grails.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
 }
--- grails-app/init/mimir/grails/BootStrap.groovy
+++ grails-app/init/mimir/grails/BootStrap.groovy
@@ -3,6 +3,20 @@ package mimir.grails
 class BootStrap {
 
     def init = { servletContext ->
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        def userRole = new Role(authority: 'ROLE_USER').save()
+
+        def adminUser = new User(username: 'admin', password: 'admin').save()
+        def playerUser = new User(username: 'player', password: 'player').save()
+
+        UserRole.create adminUser, adminRole
+        UserRole.create playerUser, userRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
     }
     def destroy = {
     }
